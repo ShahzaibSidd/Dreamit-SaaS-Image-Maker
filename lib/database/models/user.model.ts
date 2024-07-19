d38b5b2eb10ee09f915d02a99b477e4833a92f0a
Shahzaib Siddiqui <123213560+M-Shahzaib-Siddiqui@users.noreply.github.com>
2024-07-19 18:30:58 -0400

    initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,15 @@
+import { model, models, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  clerkId: {type: String, required:true, unique:true},
+  email: {type: String, required:true, unique:true},
+  username: {type: String, required:true, unique:true},
+  photo: {type:String, required:true},
+  firstName: {type:String},
+  lastName: {type:String},
+  planID: {type:Number, default:1},
+  creditBalance: {type:Number, default:10}
+});
+
+const User = models?.User || model("User", UserSchema);
+export default User;
