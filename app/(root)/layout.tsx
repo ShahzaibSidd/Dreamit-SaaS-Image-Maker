a1e8f6ec163b64aa737326eb57cee7841db23555
Shahzaib Siddiqui <123213560+M-Shahzaib-Siddiqui@users.noreply.github.com>
2024-07-26 19:03:06 -0400

    implement image transformation features

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 const Layout = ({children}: {children: React.ReactNode}) => {
@@ -8,12 +9,12 @@ const Layout = ({children}: {children: React.ReactNode}) => {
       <Sidebar/>
       <MobileNav/>
 
-
       <div className='root-container'>
         <div className='wrapper'>
           {children}
         </div>
       </div>
+      <Toaster/>
     </main> 
   )
 }
